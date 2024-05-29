8b4a3b44a53602b28f96aaf5208156df0017c994
Elwodbee <yojamshid87@gmail.com>
2024-05-29 15:15:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let newArray =[];
+
+let numbers = [76,88,99,2,6,25,62,14,22,97,43,64]
+for(i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 == 0){
+        newArray.push(numbers[i])
+    }
+}
+console.log(newArray);
+let newArra =[];
+
+let number = [55, 45, 68, 90, 5, 4, 35, 23, 77, 99, 56, 43, ]
+for(i = 0; i < number.length; i++){
+    if(number[i] % 2 == 0){
+        newArra.push(number[i])
+    }
+}
+console.log(newArra);
